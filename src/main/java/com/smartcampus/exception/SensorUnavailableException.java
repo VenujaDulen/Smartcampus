233b49266c108e3c4ac76867eb497b5309f5b6ce
Venuja2003IIT <venujathennakoon19@gmail.com>
2026-04-23 21:37:48 +0530

    update source packages

--- src/main/java/com/smartcampus/exception/SensorUnavailableException.java
+++ src/main/java/com/smartcampus/exception/SensorUnavailableException.java
@@ -2,14 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
+package com.smartcampus.exception;
 
-@Path("/test")
-public class TestResource {
-
-    @GET
-    public String test() {
-        return "API is working!";
-    }
+/**
+ *
+ * @author HP
+ */
+public class SensorUnavailableException {
+    
 }
